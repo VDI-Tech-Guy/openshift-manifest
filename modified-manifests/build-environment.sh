ba65638e00eff0043f66c19fe9e06ae8cee9d6c0
vdi-tech-guy <m.mabis@f5.com>
2024-09-06 13:42:13 -0600

    build original

--- modified-manifests/build-environment.sh
+++ modified-manifests/build-environment.sh
@@ -2,10 +2,10 @@
 oc apply -f bigip1-net-attach-ext.yaml -f bigip1-net-attach-int.yaml -f bigip1-net-attach-mgmt.yaml -f bigip1-net-attach-ha.yaml
 oc apply -f bigip2-net-attach-ext.yaml -f bigip2-net-attach-int.yaml -f bigip2-net-attach-mgmt.yaml -f bigip2-net-attach-ha.yaml
 
-#Build and Pause VM BIGIP 1
+#Build VMs
 oc apply -f bigip1-datavolume.yaml -f bigip1-vm.yaml
-while ! virtctl -n f5-bigip pause vm bigip1 ; do sleep 1 ; done
-
-#Build and Pause VM BIGIP 2 
 oc apply -f bigip2-datavolume.yaml -f bigip2-vm.yaml
+
+#Pause VMs
 while ! virtctl -n f5-bigip pause vm bigip2 ; do sleep 1 ; done
+while ! virtctl -n f5-bigip pause vm bigip1 ; do sleep 1 ; done
