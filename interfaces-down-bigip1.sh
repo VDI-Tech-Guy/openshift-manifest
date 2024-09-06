c02d802cdba015f2b62b147170b16da02d67fa98
vdi-tech-guy <m.mabis@f5.com>
2024-09-06 11:56:31 -0600

    interfaces 2 fix

--- interfaces-down-bigip1.sh
+++ interfaces-down-bigip1.sh
@@ -1,3 +1,5 @@
 LAUNCHER=$(kubectl -n f5-bigip get pods -o jsonpath='{.items[?(@.metadata.ownerReferences[].name=="bigip1")].metadata.name}')
 
 oc -n f5-bigip exec $LAUNCHER -- /bin/sh -c 'virsh domiflist 1 2>/dev/null | grep "tap" | awk "{print \$1}" | xargs -I {} virsh domif-setlink 1 {} down'
+
+oc -n f5-bigip exec $LAUNCHER -- /bin/sh -c 'virsh domiflist 1 2>/dev/null | grep "tap" | awk "{print \$1}" | xargs -I {} virsh domif-getlink 1 {}'
